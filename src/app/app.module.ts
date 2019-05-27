59b6cf3a54833eb7f3ac9612fba3c03840aab41c
jain.nikhil <jain.nikhil@YI1006637LT.Yash.com>
2019-05-27 19:30:46 +0530

    angular material stepper changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 import { DataTablesModule } from 'angular-datatables';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
@@ -19,8 +16,9 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button'
+import {MatStepperModule, MatInputModule, MatButtonModule,MatSelectModule} from '@angular/material'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -43,7 +41,10 @@ import {MatButtonModule} from '@angular/material/button'
     Ng2SmartTableModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSelectModule,
+    MatStepperModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LocationStrategy,
